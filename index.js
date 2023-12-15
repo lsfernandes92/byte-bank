af0051199eafa5a0228dd5037c696d4787bd8603
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-15 18:29:53 -0300

    Add draft commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const clientName = 'Lucas Fernandes';
+const clientCPF = 12312312312;
+const clientAgency = 0;
+const clientBalance = 0;
+
+const client2Name = 'John Doe';
+const client2CPF = 32112312312;
+const client2Agency = 0;
+const client2Balance = 0;
+
+console.log(clientName);
