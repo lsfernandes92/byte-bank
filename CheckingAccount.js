8ee2e14dc55f41932724743da524e10790dbc609
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-18 18:37:51 -0300

    Add transfer method to CA class

--- CheckingAccount.js
+++ CheckingAccount.js
@@ -1,5 +1,8 @@
+import { Client } from './Client.js'
+
 export class CheckingAccount {
   agency;
+  client;
   // The '#' is a new feature that has been approved by some JS RFC
   // and that indicates that the class attribute is private.
   // You can take a peek at: https://github.com/tc39/proposal-class-fields#private-fields
@@ -7,11 +10,26 @@ export class CheckingAccount {
   // This also means that the class attribute is private. 
   #balance = 0;
 
+  get client() {
+    return this.client;
+  }
+
+  set client(newClient) {
+    if (newClient instanceof Client) {
+      this.client = newClient;
+    }
+  }
+
+  get currentBalance() {
+    return this.#balance;
+  }
+
   withdraw(amount) {
     if (amount <= 0) return console.log('The amount must be positive!\n');
     
     if (amount <= this.#balance) {
       this.showSuccessfullyOperationMessage('withdraw', amount);
+      return true;
     } else {
       console.log('Your balance is insufficient :(\n');
     }
@@ -20,12 +38,23 @@ export class CheckingAccount {
   deposit(amount) {
     if (amount <= 0) return console.log('The amount must be positive!\n');
 
-    this.showSuccessfullyOperationMessage('deposit', amount);    
+    this.showSuccessfullyOperationMessage('deposit', amount);  
+  }
+
+  transfer(amount, fromAccount, destinationAccount) {
+    fromAccount.withdraw(amount) == true 
+      ? destinationAccount.deposit(amount) 
+      : console.log('Transfer canceled :(');
   }
 
   showSuccessfullyOperationMessage(operation, amount) {
     let operationType = '';
     operationType = operation == 'withdraw' ? 'withdrawn' : 'deposited'
+
+    console.log(`Successfully ${operationType}!`);
+    console.log(`Account owner: ${this.client.name}`);
+    console.log(`Previous balance $ ${this.currentBalance}`);
+    console.log(`${operationType.capitalize()} amount: $ ${amount}`);
 
     switch (operation) {
       case 'withdraw':
@@ -35,11 +64,8 @@ export class CheckingAccount {
         this.#balance += amount;
         break;
     }
-
-    console.log(`Successfully ${operationType}!`);
-    console.log(`Previous balance $ ${this.#balance}`);
-    console.log(`${operationType.capitalize()} amount: $ ${amount}`);
-    console.log(`Current balance $ ${this.#balance}\n`);
+    
+    console.log(`Current balance $ ${this.currentBalance}\n`);
   }
 }
 
--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import { Client } from './Client.js'
 import { CheckingAccount } from './CheckingAccount.js'
 
 const lucas = new Client();
+const foo = new Client();
 
 lucas.name = 'Lucas Fernandes';
 lucas.cpf = 12312312312;
+foo.name = 'Foo';
+foo.cpf = 11112312312;
 
 const lucasCA = new CheckingAccount();
+const fooCA = new CheckingAccount();
 
-lucasCA.agency = 0;
+lucasCA.agency = 100;
+lucasCA.client = lucas;
+fooCA.agency = 101;
+fooCA.client = foo;
+
+
 lucasCA.deposit(100);
-lucasCA.withdraw(50);
-
-console.log(lucas);
+lucasCA.transfer(10, lucasCA, fooCA);
 console.log(lucasCA);
